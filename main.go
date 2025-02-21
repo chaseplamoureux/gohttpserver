9133a16671a5368d18012e47eacc3269211dc350
Chase Lamoureux <chaseplamoureux@gmail.com>
2025-02-20 21:59:21 -0600

    Adding DB with users table and creating DB connection

--- main.go
+++ main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/chaseplamoureux/gohttpserver/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db             *database.Queries
 }
 
 func main() {
-	apiCfg := apiConfig{fileserverHits: atomic.Int32{}}
+	dbURL := os.Getenv("DB_URL")
+	db ,err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Could not connect to DB. %s\n", err)
+	}
+	dbQueries := database.New(db)
+
+	apiCfg := apiConfig{fileserverHits: atomic.Int32{}, db: dbQueries}
 
 	mux := http.NewServeMux()
 
